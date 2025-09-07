a04a74b9a05aafb373a905533be3e4052b12e422
Vaibhava Poojary <41988423+vaibhavpoojary@users.noreply.github.com>
2025-09-07 18:38:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import os
 # Initialize Flask app
 app = Flask(__name__)
 
-# Load the trained model
-model = load_model('model.h5')
+# Load the trained car model
+model = load_model('carmodel.h5')
 
 # Class labels
-class_labels = ['pituitary', 'glioma', 'notumor', 'meningioma']
+class_labels = ['Audi', 'Hyundai Creta', 'Mahindra Scorpio', 'Rolls Royce', 'Swift', 'Tata Safari', 'Toyota Innova']
 
 # Define the uploads folder
 UPLOAD_FOLDER = './uploads'
@@ -20,8 +20,8 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Helper function to predict tumor type
-def predict_tumor(image_path):
+# Helper function to predict car brand
+def predict_car(image_path):
     IMAGE_SIZE = 128
     img = load_img(image_path, target_size=(IMAGE_SIZE, IMAGE_SIZE))
     img_array = img_to_array(img) / 255.0  # Normalize pixel values
@@ -31,10 +31,8 @@ def predict_tumor(image_path):
     predicted_class_index = np.argmax(predictions, axis=1)[0]
     confidence_score = np.max(predictions, axis=1)[0]
 
-    if class_labels[predicted_class_index] == 'notumor':
-        return "No Tumor", confidence_score
-    else:
-        return f"Tumor: {class_labels[predicted_class_index]}", confidence_score
+    predicted_class = class_labels[predicted_class_index]
+    return predicted_class, confidence_score
 
 # Route for the main page (index.html)
 @app.route('/', methods=['GET', 'POST'])
@@ -47,11 +45,16 @@ def index():
             file_location = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(file_location)
 
-            # Predict the tumor
-            result, confidence = predict_tumor(file_location)
+            # Predict the car brand
+            result, confidence = predict_car(file_location)
 
             # Return result along with image path for display
-            return render_template('index.html', result=result, confidence=f"{confidence*100:.2f}%", file_path=f'/uploads/{file.filename}')
+            return render_template(
+                'index.html',
+                result=result,
+                confidence=f"{confidence*100:.2f}%",
+                file_path=f'/uploads/{file.filename}'
+            )
 
     return render_template('index.html', result=None)
 
